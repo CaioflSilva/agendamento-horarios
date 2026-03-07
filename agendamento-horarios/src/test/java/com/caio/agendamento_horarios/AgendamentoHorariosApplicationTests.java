b0f6bdabb2038f562da023724a6c2a108eb73bfe
Caio Filipe <caiofelipelg@hotmail.com>
2026-03-07 09:29:58 -0300

    Estrutura inicial do projeto de agendamento de horários

---
+++ agendamento-horarios/src/test/java/com/caio/agendamento_horarios/AgendamentoHorariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caio.agendamento_horarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoHorariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
